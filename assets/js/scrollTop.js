86a543ffc4ea744a5c0185d84b2b96bdea5851b3
jayfacemusic <66196275+jayfacemusic@users.noreply.github.com>
2025-04-06 19:34:12 +0200

    Add files via upload

---
+++ assets/js/scrollTop.js
@@ -0,0 +1,20 @@
+// scrollTop.js  /  Author: Jayface / Website: jayface.com / Version: 1.05.
+document.addEventListener('DOMContentLoaded', function() {
+  var scrollTopButton = document.getElementById('scroll-top');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+      scrollTopButton.style.display = 'block';
+    } else {
+      scrollTopButton.style.display = 'none';
+    }
+  });
+
+  scrollTopButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
