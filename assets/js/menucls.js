4aba8f704bf7c20cef9da37d070e32f221823ee5
jayfacemusic <66196275+jayfacemusic@users.noreply.github.com>
2025-07-01 01:43:05 +0200

    Delete assets directory

--- assets/js/menucls.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document / Author: Jayface / Website: jayface.com / Version: 1.05. / 2025-06-14 //
-document.querySelectorAll('.my-menu a').forEach(function(link) {
-  link.addEventListener('click', function() {
-    var toggle = document.getElementById('my-menu-toggle');
-    if (window.innerWidth <= 768 && toggle.checked) {
-      toggle.checked = false;
-    }
-  });
-});
